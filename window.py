139e878faf06c1184df757b268cfe93597dfabc0
Aska Erlangga <ae.askaerlangga@gmail.com>
2026-08-28 08:53:33 +0700

    chore: bump version to 1.2.1

--- window.py
+++ window.py
@@ -973,7 +973,7 @@ class AppUpdaterWindow(Adw.ApplicationWindow):
         about = Adw.AboutDialog(
             application_name="App Updater",
             application_icon="system-software-update",
-            version="1.2.0",
+            version="1.2.1",
             developer_name="Aska Erlangga",
             developers=["Aska Erlangga"],
             copyright="© 2026 Aska Erlangga",
